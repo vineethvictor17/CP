0a74bd4eec5b06989bd24fb82d4ebfba878a3183
vineethvictor17 <68059834+vineethvictor17@users.noreply.github.com>
2021-11-27 22:01:35 +0530

    Time: 98 ms (11.02%), Space: 125.1 MB (8.49%) - LeetHub

---
+++ jump-game-vi/jump-game-vi.java
@@ -0,0 +1,22 @@
+class Solution {
+    public int maxResult(int[] nums, int k) {
+        if(nums.length == 0) return 0;
+        
+    
+        Queue<int[]> pq = new PriorityQueue<>((a,b)->(b[0]-a[0]));
+        pq.offer(new int[] {nums[0],0});
+        int max=nums[0];
+        
+        for(int i=1;i<nums.length;i++){
+            while(pq.peek()[1] < i-k){
+               pq.poll();
+            }
+            
+            int[] cur=pq.peek();
+            max = cur[0] + nums[i];
+            pq.offer(new int[] {max,i});
+        }
+        return max;
+        
+    }
+}
