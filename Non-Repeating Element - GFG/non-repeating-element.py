5b89fcb666e3462dc827b258ea59cbbed93675f0
vineethvictor17 <68059834+vineethvictor17@users.noreply.github.com>
2022-06-04 20:23:01 +0530

    Added solution - LeetHub

---
+++ Non-Repeating Element - GFG/non-repeating-element.py
@@ -0,0 +1,26 @@
+#User function Template for python3
+
+class Solution:
+    def firstNonRepeating(self, arr, n):
+        # Complete the function
+       mp = defaultdict(lambda:0)
+       for i in range(n):
+           mp[arr[i]]+=1
+    
+       for i in range(n):
+           if mp[arr[i]] == 1:
+               return arr[i]
+
+#{ 
+#  Driver Code Starts
+#Initial Template for Python 3
+
+from collections import defaultdict 
+
+for _ in range(0,int(input())):
+    n = int(input())
+    arr = list(map(int,input().strip().split()))
+    ob = Solution()
+    print(ob.firstNonRepeating(arr, n))
+    
+# } Driver Code Ends
